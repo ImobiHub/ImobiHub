403d7927f5d094db70b105049a7c8fd945e88048
Otávio Santana Possenti <otavio@wetzel.net>
2026-03-25 17:35:27 -0300

    Fix: ajustada a versão do ChakraUi que estava ocasionando bugs bugs no código

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
-import { Box, Button, Heading } from "@chakra-ui/react";
+import { Box, Heading, Text, Button } from "@chakra-ui/react";
 
 function App() {
   return (
     <Box p={8}>
-      <Heading>Chakra UI funcionando!</Heading>
-      <Button mt={4} colorScheme="teal">
-        Clique aqui
-      </Button>
+      <Heading>ImobiHub - Frontend</Heading>
+      <Text mt={4}>Primeira página React com Chakra UI funcionando!</Text>
+      <Button mt={4} colorScheme="teal">Clique aqui</Button>
     </Box>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
-import App from "./App";
-import theme from "./theme/theme";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
+import './index.css'
+import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
